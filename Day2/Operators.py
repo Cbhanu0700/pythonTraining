0ab8eb78a195b15fe6f12fcdeef65258316035c6
Cbhanu0700 <chandubhanuprakash123@gmail.com>
2025-04-21 17:29:38 +0530

    21/04/2025 Bhanu QA First Commit

---
+++ Day2/Operators.py
@@ -0,0 +1,31 @@
+# Arthmatic Operators
+from operator import truediv
+
+# a=100
+# b=120
+# print(a+b) #Addition
+# print(a-b)#Subtraction
+# print(a*b)#Multiplication
+# print(a/b)#Quotient with decimal
+# print(a//b)#Quotient without decimal
+# print(a%b)#Remainder
+
+#Relational/Comparison Operators
+
+# c=111
+# d=222
+# print(c>d)
+# print(c<d)
+# print(c>=d)
+# print(c<=d)
+# print(c==d)
+# print(c!=d)
+
+#Logical Operators
+
+a=True
+b=False
+
+print( a and b)
+print(a or b)
+print(not a)
